fece7eb0a64723bfbb1d4b72c3e3e6b557492cf0
tjark <tjark.ziehm@ohioh.de>
2023-06-23 13:11:21 +0200

    Upload

---
+++ cookieServer.py
@@ -0,0 +1,76 @@
+import socket
+# 192.168.178.100
+host = '192.168.178.101'
+port = 9100
+
+storedValue = "Yo, what's up?"
+
+def setupServer():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socket created.")
+    try:
+        s.bind((host, port))
+    except socket.error as msg:
+        print(msg)
+    print("Socket bind comlete.")
+    print("Done for",host,port)
+    return s
+
+def setupConnection():
+    print("listening...")
+    s.listen(1) # Allows one connection at a time.
+    
+    print("accept input")
+    conn, address = s.accept()
+    print("Connected to: " + address[0] + ":" + str(address[1]))
+    return conn
+
+def GET():
+    reply = storedValue
+    return reply
+
+def REPEAT(dataMessage):
+    reply = dataMessage[1]
+    return reply
+
+def dataTransfer(conn):
+    # A big loop that sends/receives data until told not to.
+    while True:
+        # Receive the data
+        data = conn.recv(1024) # receive the data
+        data = data.decode('utf-8')
+        # Split the data such that you separate the command
+        # from the rest of the data.
+        dataMessage = data.split(' ', 1)
+        command = dataMessage[0]
+        if command == 'GET':
+            reply = GET()
+        elif command == 'REPEAT':
+            reply = REPEAT(dataMessage)
+        elif command == 'EXIT':
+            print("Our client has left us :(")
+            break
+        elif command == 'KILL':
+            print("Our server is shutting down.")
+            s.close()
+            break
+        else:
+            reply = 'Unknown Command'
+        # Send the reply back to the client
+        conn.sendall(str.encode(reply))
+        print("Data has been sent!")
+    conn.close()
+        
+
+s = setupServer()
+
+while True:
+    try:
+        print("try:")
+        conn = setupConnection()
+        print("setup done")
+        dataTransfer(conn)
+        print("loop")
+    except:
+        print("break")
+        break
---
+++ echo.py
@@ -0,0 +1,25 @@
+# echo-server.py
+
+import socket
+
+HOST = "127.0.0.1"
+# Standard loopback interface address (localhost)
+PORT =  65432
+#65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #s.close()
+    s.bind((HOST, PORT))
+    print("binded")
+    s.listen()
+    print("listening")
+    conn, addr = s.accept()
+    print("connecting:")
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+            print(data)
